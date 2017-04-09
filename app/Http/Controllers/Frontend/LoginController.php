cc0bf437dc0e4fff38f75ed2fd54847e1057f2e9
Javed Ahmad <javed0089@gmail.com>
2017-04-09 10:24:43 +0300

    login

--- app/Http/Controllers/Frontend/LoginController.php
+++ app/Http/Controllers/Frontend/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
         	
         	$this->validate($request, [
         	'email' => 'required|email',
-    		'password' => 'required|min:5|max:10',
+    		'password' => 'required|min:6',
     		]);
 
 
--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -40,29 +40,24 @@
                                    
                                     <!-- Email input-->
                                     <div class="form-group">
-                                        <input name="email" placeholder="{{__('Email Address')}}" class="form-control" type="email" required title="Please enter your email address">
+                                        <input name="email" placeholder="{{__('Email Address')}}" class="form-control" type="email" required data-parsley-required-message="Please enter your email address">
                                     </div>
                                     <!-- Password-->
                                     <div class="form-group">
-                                        <input name="password" placeholder="{{__('Password')}}" class="form-control" type="password" required minlength="6" title="Please enter your password" >
+                                        <input name="password" placeholder="{{__('Password')}}" class="form-control" type="password" required minlength="6" data-parsley-required-message="Please enter your password" >
                                     </div>
                                     <a href="{{route('frontend.forgot-password')}}">Forgot password?</a>
-                                    <div class="checkbox pull-right">
-                                        <label><input name=rememberMe style="margin-top: .5px;" type="checkbox" value="">Remeber me</label>
+                                    <div class=" pull-right">
+                                        <label><input name="rememberMe"  type="checkbox"> Remeber me</label>
                                     </div>
                                     
-                                   
-
                                     <!-- Button -->
                                     <button type="submit" class="btn btn-block btn-warning" id="js-contact-btn"> {{__('LOGIN')}} </button>
                                     <div class="spacer-30"> </div>
                                         
-                                   
-
-
-                                    <div id="js-contact-result" data-success-msg="Form submitted successfully." data-error-msg="Oops. Something went wrong."></div>
-
                                 </form>
+
+
                             </div>
                         </div>
                      
