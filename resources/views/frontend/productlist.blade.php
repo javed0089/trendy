69d5a6b5d50dbf6a54fe31e1fc7a4b135c15d61b
Javed Ahmad <javed0089@gmail.com>
2017-04-24 17:50:03 +0300

    sidebar

--- resources/views/frontend/productlist.blade.php
+++ resources/views/frontend/productlist.blade.php
@@ -40,8 +40,7 @@
              @endif
            </a>
            <h4 class="subtitle services-title-one"><a href="{{route('frontend.product',$product->slug)}}"> {{$product->{lang_col('name')} }}</a></h4>
-           <div>{!!str_limit($product->{lang_col('desc')},180)!!} </div>
-           
+           <p>{!!str_limit($product->{lang_col('desc')},180)!!} </p>
            <input type="text" hidden value="{{route('addToCart',$product->id)}}" name="">
            <a  class="btn btn-danger btn-sm quote" href="#"> {{__('Request A Quote')}} <img id="loader" class="pull-right" width="35" style="display: none;" src="{{asset('images/ellipsis.gif')}}" alt="loading"></a>
            <div id="alert" style="display: none; margin-top: 5px;" >
@@ -68,8 +67,7 @@
              @endif
            </a>
            <h4 class="subtitle services-title-one"><a href="{{route('frontend.product',$product->slug)}}"> {{$product->{lang_col('name')} }}</a></h4>
-           <div>{!!str_limit($product->{lang_col('desc')},180)!!} </div>
-           <!--  <a class="link" href="{{route('frontend.product',$product->slug)}}"> Read more </a>-->
+           <p>{!!str_limit($product->{lang_col('desc')},180)!!} </p>
            <input type="text" hidden value="{{route('addToCart',$product->id)}}" name="">
            <a  class="btn btn-danger btn-sm quote" href="#"> {{__('Request A Quote')}} <img id="loader" class="pull-right" width="35" style="display: none;" src="{{asset('images/ellipsis.gif')}}" alt="loading"></a>
            <div id="alert" style="display: none; margin-top: 5px;" >
