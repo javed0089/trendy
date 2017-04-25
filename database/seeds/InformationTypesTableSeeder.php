53ea90aa3b1aa13d38c56a2759ca240420aa5238
Javed Ahmad <javed0089@gmail.com>
2017-04-25 17:25:44 +0300

    4th quality check

--- database/seeds/InformationTypesTableSeeder.php
+++ database/seeds/InformationTypesTableSeeder.php
@@ -16,6 +16,7 @@ class InformationTypesTableSeeder extends Seeder
     	DB::table('information_types')->insert([
     		['id' => '1','slug'=>'privacy-policy','information_type_en' => 'Privacy Policy','information_type_ar' => 'سياسة الخصوصية'],
     		['id' => '2','slug'=>'terms-of-use','information_type_en' => 'Terms of use','information_type_ar' => 'تعليمات الاستخدام'],
+            ['id' => '3','slug'=>'disclaimer','information_type_en' => 'Disclaimer','information_type_ar' => 'تنصل'],
     		]);
     }
 }
