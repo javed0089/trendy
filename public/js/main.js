ed72f39f2296eb5ff2a1799e8709ad41a43a7ecb
Javed Ahmad <javed0089@gmail.com>
2017-04-29 09:21:55 +0300

    4th quality check

--- public/js/main.js
+++ public/js/main.js
@@ -261,11 +261,11 @@ Version: 1.0
             else{
               me.next('#alert').addClass('ajax-success');
 
-              /*$('#side-cart-table > tbody:last-child').append(
+             $('#side-cart-table > tbody:last-child').append(
                 '<tr id='+ data.cartItem['item']['id'] +'><td><strong>'+ data.cartItem['item']['name_en'] +
                 '</strong></td><td>'+ data.cartItem['quantity'] + '</td><td>MTN</td></tr>');
 
-              $('#cartNoProducts').hide();
+              /* $('#cartNoProducts').hide();
               if($('#updateCart').length==0)
                 $('#side-cart-table').parent().append('<div id=\"updateCart\" class=\"center-block text-center\">'+
                   '<a class=\"btn btn-primary btn-sm\" href=\"/cart\">Update Cart</a></div>'); */ 
