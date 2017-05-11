03eaf66aeb5a8a71d8e9d61721f93f9bb9a5a2b3
Javed Ahmad <javed0089@gmail.com>
2017-05-11 13:36:50 +0300

    notifications

--- resources/views/backend/partials/_adminfooter.blade.php
+++ resources/views/backend/partials/_adminfooter.blade.php
@@ -3,6 +3,5 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.3.8
     </div>
-    <strong>Copyright &copy; {{date('Y')}} <a href="http://gap-polymers.com">GAP Polymers</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; {{date('Y')}} <a href="http://gap-polymers.com">GAP Polymers</a>.</strong> All rights reserved.
 </footer>
